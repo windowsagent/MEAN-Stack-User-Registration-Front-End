b0a57ac06a71a050325d84d5e7770e443235fb5b
windowsagent <juanjphc@gmail.com>
2024-10-16 13:17:03 -0600

    Fix CORS + api URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  apiBaseUrl: `http://${location.origin}/api`
+  apiBaseUrl: location.origin + '/api'
 };
 
 /*
